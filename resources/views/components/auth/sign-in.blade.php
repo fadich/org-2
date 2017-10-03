2022cdc396a4b9f4fbb40855da9db85c53800eb1
Alexander Strelnik <alexander.strelnik@ddi-dev.com.ua>
2017-10-03 17:55:55 +0300

    ``` base layout

--- resources/views/components/auth/sign-in.blade.php
+++ resources/views/components/auth/sign-in.blade.php
@@ -11,7 +11,7 @@
                 {{ csrf_field() }}
 
                 <div class="form-group">
-                    <input class="form-control" type="text" name="email" placeholder="Login" required>
+                    <input class="form-control" type="text" name="email" placeholder="Login or Email" required>
                 </div>
 
                 <div class="form-group">
