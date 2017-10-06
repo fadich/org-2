443977c322a11f65547047f8641223dbc928561d
fadich <royalfadich@gmail.com>
2017-10-06 21:29:31 +0300

    ``` init document repository project

---
+++ frontend/document-repository/src/components/Ckeditor.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="app">
+        <ckeditor
+                v-model="content"
+                :config="config"
+                @blur="onBlur($event)"
+                @focus="onFocus($event)">
+        </ckeditor>
+    </div>
+</template>
+
+<script>
+  import Ckeditor from 'vue-ckeditor2'
+
+  export default {
+    components: {Ckeditor},
+    data () {
+      return {
+        content: '',
+        config: {
+          toolbar: [
+            { name: 'document', items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+            { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+            { name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+            { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+            '/',
+            { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
+            { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+            { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+            { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+            '/',
+            { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+            { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+            { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] }
+          ],
+          height: '75vh'
+        }
+      }
+    },
+    methods: {
+      onBlur (editor) {
+        console.log(editor)
+      },
+      onFocus (editor) {
+        console.log(editor)
+      }
+    }
+  }
+</script>
