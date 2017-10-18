993a0b7421976b8c1f00ce0cf0a5b3d0dbd18985
fadich <royalfadich@gmail.com>
2017-10-18 22:35:40 +0300

    ``` fullscreen

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -30,6 +30,28 @@
 
     Vue.component("app-footer", {
         template: "#app-footer",
+        methods: {
+            toggleFullScreen() {
+                if ((document.fullScreenElement && document.fullScreenElement !== null) ||
+                    (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+                    if (document.documentElement.requestFullScreen) {
+                        document.documentElement.requestFullScreen();
+                    } else if (document.documentElement.mozRequestFullScreen) {
+                        document.documentElement.mozRequestFullScreen();
+                    } else if (document.documentElement.webkitRequestFullScreen) {
+                        document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+                    }
+                } else {
+                    if (document.cancelFullScreen) {
+                        document.cancelFullScreen();
+                    } else if (document.mozCancelFullScreen) {
+                        document.mozCancelFullScreen();
+                    } else if (document.webkitCancelFullScreen) {
+                        document.webkitCancelFullScreen();
+                    }
+                }
+            }
+        },
     });
 
     let homepage = {
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,6 +4,7 @@
             <li>Item 1</li>
             <li>Item 2</li>
             <li>Item 3</li>
+            <a @click="toggleFullScreen">FS</a>
         </ul>
     </footer>
 </template>
