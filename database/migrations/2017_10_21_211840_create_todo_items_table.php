cd459eaa40c1d9d51b47156e652ff28d8bd09fed
Fadi A <royalfadich@gmail.com>
2017-10-22 00:46:50 +0300

    ``` Todo bundle > migration > plural

--- database/migrations/2017_10_21_211840_create_todo_items_table.php
+++ database/migrations/2017_10_21_211840_create_todo_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTodoItemTable extends Migration
+class CreateTodoItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTodoItemTable extends Migration
      */
     public function up()
     {
-        Schema::create('todo_item', function (Blueprint $table) {
+        Schema::create('todo_items', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
             $table->string('content');
