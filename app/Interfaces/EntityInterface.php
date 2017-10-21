3d061f9c5f2562778ed32306a4b9b759145f4a0f
fadich <royalfadich@gmail.com>
2017-10-22 00:30:11 +0300

    ``` Todo bundle > base

--- app/Interfaces/EntityInterface.php
+++ app/Interfaces/EntityInterface.php
@@ -8,8 +8,18 @@ namespace App\Interfaces;
  *
  * Entity is a directly data-collection object.
  *
+ * @property integer|string $id
+ *
  * @author Fadi Ahmad <royalfadich@gmail.com>
  */
 interface EntityInterface
 {
+    /**
+     * Getter for the entity identifier.
+     *
+     * @return string|integer
+     *   The identifier.
+     */
+    public function getId();
+
 }
