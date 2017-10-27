7398f41fe7f74834e56482371d44cd1190926593
Fadi A <royalfadich@gmail.com>
2017-10-28 02:48:54 +0300

    ``` Ajax middleware

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", ["uses" => "HomeController@indexAction", "as" => "home"]);
 
-Route::get("/login", function (){
+Route::get("/login", function () {
     return redirect("/#sign-in?land-to=home");
 })->name("login")->middleware("guest");
 
@@ -23,14 +23,23 @@ Route::get("/login", function (){
 Route::group(["prefix" => "auth"], function () {
     Route::post("/sign-in", ["uses" => "Auth\LoginController@loginAction", "as" => "auth.login"])->middleware("guest");
 
-    Route::post("/sign-up", ["uses" => "Auth\RegisterController@registerAction", "as" => "auth.register"])->middleware("guest");
+    Route::post("/sign-up", ["uses" => "Auth\RegisterController@registerAction", "as" => "auth.register"])
+        ->middleware("guest");
 
-    Route::get("/sign-out", ["uses" => "Auth\LoginController@logoutAction", "as" => "auth.sign-out"])->middleware("auth");
+    Route::get("/sign-out", ["uses" => "Auth\LoginController@logoutAction", "as" => "auth.sign-out"])
+        ->middleware("auth");
 });
 
 Route::group(["prefix" => "todo"], function () {
-    Route::get("/", ["uses" => "Todo\TodoController@indexAction", "as" => "todo.all"]);
-    Route::put("/item", ["uses" => "Todo\TodoController@itemAction", "as" => "todo.item.create"]);
-    Route::put("/item/{id}", ["uses" => "Todo\TodoController@itemAction", "as" => "todo.item.update"]);
-    Route::delete("/item/{id}", ["uses" => "Todo\TodoController@deleteAction", "as" => "todo.item.delete"]);
+    Route::get("/", ["uses" => "Todo\TodoController@indexAction", "as" => "todo.all"])
+        ->middleware("ajax");
+
+    Route::put("/item", ["uses" => "Todo\TodoController@itemAction", "as" => "todo.item.create"])
+        ->middleware("ajax");
+
+    Route::put("/item/{id}", ["uses" => "Todo\TodoController@itemAction", "as" => "todo.item.update"])
+        ->middleware("ajax");
+
+    Route::delete("/item/{id}", ["uses" => "Todo\TodoController@deleteAction", "as" => "todo.item.delete"])
+        ->middleware("ajax");
 });
